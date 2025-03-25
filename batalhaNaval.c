08be887555629bc0619ed0999d724c701743bcf3
Quati-de-Cartola <vitor.bryan.santos@gmail.com>
2025-03-25 18:44:31 +0000

    Comecando 2

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,55 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    int tabuleiro[10][10] = {
-        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {3, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {4, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {5, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {6, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {7, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {8, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {9, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    int tabuleiro[10][10] = {0}; // Inicializa toda a matriz com 0
+
+    printf("TABULEIRO BATALHA NAVAL\n\n");
+
+    // Imprimir índices da coluna (cabeçalho)
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf("%d ", j);
     }
+    printf("\n");
 
-    printf("TABULEIRO BATALHA NAVAL\n");
-
-    for(int i = 0; i <= 10; i++) {
-        printf("%d", tabuleiro[i][i]);
+    // Imprimir o tabuleiro
+    for (int i = 0; i < 10; i++) {
+        printf("%d  ", i); // Índice da linha
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]); // Imprimir cada célula do tabuleiro
+        }
+        printf("\n");
     }
-
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
