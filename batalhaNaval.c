ffdaef1902b752eaffac7e2b572f6d80a580a92a
Quati-de-Cartola <vitor.bryan.santos@gmail.com>
2025-03-25 18:51:12 +0000

    Melhorando - navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,7 +2,17 @@
 
 int main() {
     int tabuleiro[10][10] = {0}; // Inicializa toda a matriz com 0
-    
+
+    // Posicionando navio na horizontal
+    tabuleiro[2][4] = 1;
+    tabuleiro[2][5] = 1;
+    tabuleiro[2][6] = 1;
+
+    // Posicionando navio na vertical
+    tabuleiro[0][1] = 1;
+    tabuleiro[1][1] = 1;
+    tabuleiro[2][1] = 1;
+
     printf("TABULEIRO BATALHA NAVAL\n\n");
 
     // Imprimir índices da coluna (cabeçalho)
