99fd6f8ff62233d8e674a58d3987fd4886bf19b8
Quati-de-Cartola <vitor.bryan.santos@gmail.com>
2025-03-31 17:45:57 +0000

    Atualizando para Constantes

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 
+// Algumas constantes
+#define LINHAS  = 10;
+#define COLUNAS = 10;
+
 int main() {
-    int tabuleiro[10][10] = {0}; // Inicializa toda a matriz com 0
-
-    // Posicionando navio na horizontal
-    tabuleiro[2][4] = 1;
-    tabuleiro[2][5] = 1;
-    tabuleiro[2][6] = 1;
-
-    // Posicionando navio na vertical
-    tabuleiro[0][1] = 1;
-    tabuleiro[1][1] = 1;
-    tabuleiro[2][1] = 1;
+    int tabuleiro[LINHAS][COLUNAS] = {0}; // Inicializa toda a matriz com 0
 
     printf("TABULEIRO BATALHA NAVAL\n\n");
 
     // Imprimir índices da coluna, de A-J como uma planilha do excel
     printf("   ");
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < LINHAS; j++) {
         printf("%c ", 'A' + j); // Sp,amdo um caractere com J, fazendo ele seguir um padrão de A - J
     }
     printf("\n");
 
     // Imprimir o tabuleiro
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < LINHAS; i++) {
         printf("%d  ", i); // Índice da linha
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < COLUNAS; j++) {
             printf("%d ", tabuleiro[i][j]); // Imprimir cada célula do tabuleiro
         }
         printf("\n");
